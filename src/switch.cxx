8700516a79f12d330bb23859525fe287a6f4e921
Elkeid-me <elkeid.me@gmail.com>
2023-12-30 12:17:49 +0800

    最终版本，使用 `USE_STD_FUNCTIONS` 控制标准函数的使用

--- src/switch.cxx
+++ src/switch.cxx
@@ -1,33 +1,39 @@
 #include "switch.hxx"
 #include "types.hxx"
+#include <algorithm>
 #include <bit>
 #include <cstddef>
 #include <cstdint>
-// #include <cstring>
-// #include <iomanip>
-// #include <numeric>
-// #include <sstream>
-// #include <string>
-// #include <string_view>
+#include <cstring>
+#include <functional>
+#include <iomanip>
+#include <numeric>
+#include <sstream>
+#include <string>
+#include <string_view>
+#include <utility>
+
+// #define USE_STD_FUNCTIONS
 
 SwitchBase *CreateSwitchObject() { return new Switch; }
 
 int PackFrame(char *unpacked_frame, char *packed_frame, int frame_length)
 {
-    // std::ostringstream os(std::ios::binary);
-    // os << std::quoted<char>(std::string_view(unpacked_frame, frame_length), FRAME_DELI,
-    //                         FRAME_DELI);
-    // std::string s{os.str()};
-    // std::uint8_t xor_result{static_cast<uint8_t>(std::reduce(
-    //     s.begin(), s.end() - 1, '\0', [](char lhs, char rhs) { return lhs ^ rhs; }))};
-    // s.back() = std::popcount(xor_result) % 2 != 0 ? '\1' : '\0';
-    // std::memcpy(packed_frame, s.c_str(), s.length());
-    // return s.length();
+#ifdef USE_STD_FUNCTIONS
+    std::ostringstream os(std::ios::binary);
+    os << std::quoted<char>(std::string_view(unpacked_frame, frame_length), FRAME_DELI,
+                            FRAME_DELI);
+    std::string s{os.str()};
+    std::uint8_t xor_result{static_cast<uint8_t>(
+        std::reduce(s.begin(), s.end() - 1, '\0', std::bit_xor<char>()))};
+    s.back() = std::popcount(xor_result) % 2 != 0 ? '\1' : '\0';
+    std::memcpy(packed_frame, s.c_str(), s.length());
+    return s.length();
+#else
     std::uint8_t *packed_ptr{reinterpret_cast<std::uint8_t *>(packed_frame)};
     std::uint8_t *unpacked_ptr{reinterpret_cast<std::uint8_t *>(unpacked_frame)};
     std::size_t unpacked_size{static_cast<size_t>(frame_length)};
     std::uint8_t xor_result{FRAME_DELI};
-
     packed_ptr[0] = FRAME_DELI;
     std::size_t process_ptr{1};
     for (std::size_t i{0}; i < unpacked_size; i++)
@@ -45,41 +51,37 @@ int PackFrame(char *unpacked_frame, char *packed_frame, int frame_length)
             process_ptr++;
         }
     }
-    if (std::popcount(xor_result) % 2 != 0)
-        packed_ptr[process_ptr] = '\1';
-    else
-        packed_ptr[process_ptr] = '\0';
-
+    packed_ptr[process_ptr] = std::popcount(xor_result) % 2 != 0 ? '\1' : '\0';
     process_ptr++;
     return process_ptr;
+#endif
 }
 
 int UnpackFrame(char *unpacked_frame, char *packed_frame, int frame_length)
 {
-    // if (static_cast<std::uint8_t>(packed_frame[0]) != FRAME_DELI)
-    //     return -1;
-    // std::uint8_t xor_result{
-    //     static_cast<uint8_t>(std::reduce(packed_frame, packed_frame + frame_length, '\0',
-    //                                      [](char lhs, char rhs) { return lhs ^ rhs; }))};
-    // if (std::popcount(xor_result) % 2 != 0)
-    //     return -1;
-    // std::istringstream is(std::string(packed_frame, frame_length));
-    // std::string result;
-    // is >> std::quoted<char>(result, FRAME_DELI, FRAME_DELI);
-    // if (!is.eof())
-    //     return -1;
-    // std::memcpy(unpacked_frame, result.c_str(), result.length() - 1);
-    // return result.length() - 1;
+#ifdef USE_STD_FUNCTIONS
+    if (static_cast<std::uint8_t>(packed_frame[0]) != FRAME_DELI)
+        return -1;
+    std::uint8_t xor_result{static_cast<uint8_t>(std::reduce(
+        packed_frame, packed_frame + frame_length, '\0', std::bit_xor<char>()))};
+    if (std::popcount(xor_result) % 2 != 0)
+        return -1;
+    std::istringstream is(std::string(packed_frame, frame_length), std::ios::binary);
+    std::string result;
+    is >> std::quoted<char>(result, FRAME_DELI, FRAME_DELI);
+    if (!is.eof())
+        return -1;
+    std::memcpy(unpacked_frame, result.c_str(), result.length() - 1);
+    return result.length() - 1;
+#else
     std::uint8_t *packed_ptr{reinterpret_cast<std::uint8_t *>(packed_frame)};
     std::uint8_t *unpacked_ptr{reinterpret_cast<std::uint8_t *>(unpacked_frame)};
     std::size_t packed_size{static_cast<size_t>(frame_length - 1)};
     std::size_t process_ptr{0};
     bool de_flag{false};
     std::uint8_t xor_result{FRAME_DELI};
-
     if (packed_ptr[0] != FRAME_DELI)
         return -1;
-
     for (std::size_t i{1}; i < packed_size; i++)
     {
         xor_result ^= packed_ptr[i];
@@ -99,23 +101,37 @@ int UnpackFrame(char *unpacked_frame, char *packed_frame, int frame_length)
             process_ptr++;
         }
     }
-
     xor_result ^= packed_ptr[packed_size];
-
     if (std::popcount(xor_result) % 2 != 0)
         return -1;
     return process_ptr;
+#endif
 }
 
 void Switch::InitSwitch(int numPorts) { ports = numPorts; }
 
 int Switch::ProcessFrame(int inPort, char *framePtr)
 {
+    if (inPort < 1 || inPort > ports)
+        return -1;
     ether_header *header{reinterpret_cast<ether_header *>(framePtr)};
-    if (header->get_type() == ETHER_CONTROL_TYPE)
+    std::uint64_t src{header->get_src()}, dst{header->get_dst()};
+    if (src == dst)
+        return -1;
+    auto outport_iter{table.find(dst)};
+    switch (header->get_type())
     {
+    case ETHER_CONTROL_TYPE:
         if (header->get_length() == 0 && inPort == 1)
         {
+#ifdef USE_STD_FUNCTIONS
+            std::ranges::for_each(
+                table, [](std::pair<const std::uint64_t, std::pair<int, int>> &p)
+                { p.second.second--; });
+            std::erase_if(table,
+                          [](std::pair<const std::uint64_t, std::pair<int, int>> &p)
+                          { return p.second.second == 0; });
+#else
             auto iter{table.begin()}, end{table.end()};
             while (iter != end)
             {
@@ -125,18 +141,15 @@ int Switch::ProcessFrame(int inPort, char *framePtr)
                 else
                     ++iter;
             }
+#endif
         }
-        return -1;
+        break;
+    case ETHER_DATA_TYPE:
+        table.insert_or_assign(src, std::pair{inPort, 10});
+        if (outport_iter == table.end())
+            return 0;
+        if (outport_iter->second.first != inPort)
+            return outport_iter->second.first;
     }
-
-    std::uint64_t src{header->get_src()}, dst{header->get_dst()};
-    auto outport_iter{table.find(dst)};
-    table[src] = {inPort, 10};
-
-    if (outport_iter == table.end())
-        return 0;
-
-    if (outport_iter->second.first == inPort)
-        return -1;
-    return outport_iter->second.first;
+    return -1;
 }
